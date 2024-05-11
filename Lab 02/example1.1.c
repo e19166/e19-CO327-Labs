dc3683cd4595e16f5e0dae90ef6f6474b5a645a3
e19166 <e19166@eng.pdn.ac.lk>
2024-05-11 14:29:09 +0530

    simple program to get familiar with basic file functions in c added

---
+++ Lab 02/example1.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+
+int main() {
+  char* banner = "This is a string written to a textfile by a C program\n";
+  int desc = open("out.txt", O_WRONGLY|O_APPEND|O_CREATE, S_IRUSR | S_IWUSR);
+  write(desc, banner, strlen(banner));
+  close(desc);
+  return 0;
+}
